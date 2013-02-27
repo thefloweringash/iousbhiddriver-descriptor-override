b5749dcdbebb13afb5c83862e7cec999814b0e11
Andrew Childs <lorne@cons.org.nz>
2013-02-27 23:47:12 +1300

    scanner: Ignore errors while fetching descriptor
    Not all devices will respond to our attempts to read their report
    descriptor. This shouldn't terminate the entire scan.

--- scripts/scanner.rb
+++ scripts/scanner.rb
@@ -9,6 +9,19 @@ require 'pp'
 $LOAD_PATH << File.dirname(__FILE__)
 require 'hidutil'
 
+def get_hid_report_descriptor(devh, intf)
+  hid_descriptor = devh.control_transfer(:bmRequestType =>  LIBUSB::ENDPOINT_IN | LIBUSB::RECIPIENT_INTERFACE,
+                                         :bRequest => LIBUSB::REQUEST_GET_DESCRIPTOR,
+                                         :wValue => (LIBUSB::DT_HID << 8) | 0,
+                                         :wIndex => intf.bInterfaceNumber,
+                                         :dataIn => 9)
+  report_descriptor_length = hid_descriptor[7..9].unpack('v').first
+  devh.control_transfer(:bmRequestType =>  LIBUSB::ENDPOINT_IN | LIBUSB::RECIPIENT_INTERFACE,
+                        :bRequest => LIBUSB::REQUEST_GET_DESCRIPTOR,
+                        :wValue => (LIBUSB::DT_REPORT << 8) | 0,
+                        :wIndex => intf.bInterfaceNumber,
+                        :dataIn => report_descriptor_length)
+end
 usb = LIBUSB::Context.new
 usb.devices(:bClass => LIBUSB::CLASS_HID).each do |dev|
   dev.open do |devh|
@@ -19,31 +32,25 @@ usb.devices(:bClass => LIBUSB::CLASS_HID).each do |dev|
     ambiguous_descriptors = []
 
     dev.interfaces.each do |intf|
-      default_settings = intf.settings.first
-      next if default_settings.bInterfaceClass != LIBUSB::CLASS_HID
-      hid_descriptor = devh.control_transfer(:bmRequestType =>  LIBUSB::ENDPOINT_IN | LIBUSB::RECIPIENT_INTERFACE,
-                                             :bRequest => LIBUSB::REQUEST_GET_DESCRIPTOR,
-                                             :wValue => (LIBUSB::DT_HID << 8) | 0,
-                                             :wIndex => intf.bInterfaceNumber,
-                                             :dataIn => 9)
-      report_descriptor_length = hid_descriptor[7..9].unpack('v').first
-      report_descriptor = devh.control_transfer(:bmRequestType =>  LIBUSB::ENDPOINT_IN | LIBUSB::RECIPIENT_INTERFACE,
-                                                :bRequest => LIBUSB::REQUEST_GET_DESCRIPTOR,
-                                                :wValue => (LIBUSB::DT_REPORT << 8) | 0,
-                                                :wIndex => intf.bInterfaceNumber,
-                                                :dataIn => report_descriptor_length)
-      items = HIDInfo.parse(StringIO.new report_descriptor)
+      begin
+        default_settings = intf.settings.first
+        next if default_settings.bInterfaceClass != LIBUSB::CLASS_HID
+        report_descriptor = get_hid_report_descriptor(devh, intf)
+        items = HIDInfo.parse(StringIO.new report_descriptor)
 
-      if items.any? &:is_ambiguous?
-        STDERR.puts "Found ambiguity!"
-        ambiguous_descriptors << {
-          'idVendor' => dev.idVendor,
-          'idProduct' => dev.idProduct,
-          'bcdDevice' => dev.bcdDevice,
-          'bConfigurationValue' => default_settings.configuration.bConfigurationValue,
-          'bInterfaceNumber' => intf.bInterfaceNumber,
-          'hidReportDescriptor' => report_descriptor,
-        }
+        if items.any? &:is_ambiguous?
+          STDERR.puts "Found ambiguity!"
+          ambiguous_descriptors << {
+            'idVendor' => dev.idVendor,
+            'idProduct' => dev.idProduct,
+            'bcdDevice' => dev.bcdDevice,
+            'bConfigurationValue' => default_settings.configuration.bConfigurationValue,
+            'bInterfaceNumber' => intf.bInterfaceNumber,
+            'hidReportDescriptor' => report_descriptor,
+          }
+        end
+      rescue => e
+        STDERR.puts "Ignoring exception \"#{e}\" processing #{intf.inspect}"
       end
     end
     if !ambiguous_descriptors.empty?
